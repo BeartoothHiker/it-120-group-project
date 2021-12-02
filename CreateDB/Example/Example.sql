9e1a2b26177812b5ba91a7c33fdebd66b9ec1c9a
Jeremy R <72467083+BeartoothHiker@users.noreply.github.com>
2021-12-02 11:34:52 -0800

    Final pass through schema. Remove 'not-null' requirements on some table fields.  Add some default values (for timestamps)

--- CreateDB/Example/Example.sql
+++ CreateDB/Example/Example.sql
@@ -1,7 +1,26 @@
 
-drop table member;
+/*
+-- Run this portion of the script to drop tables
+-- if we make a change to the schema.
+
+-- Warning Drop tables will delete table & all data.
+
+drop table Donation;
+drop table SaleItem;
+drop table Sale;
+
+drop table EventComment;
+drop table ResourceComment;
+drop table Comment;
+
+drop table Event;
+drop table Resource;
+
+drop table member cascade;
 drop table person cascade;
 drop table address cascade;
+
+*/
 
 CREATE TABLE person
 (
@@ -11,8 +30,6 @@ CREATE TABLE person
 	email text not null,
 	phone text
 );
-
-
 
 CREATE TABLE member
 (
@@ -34,70 +51,56 @@ CREATE TABLE address
 );
 
 
-CREATE TABLE Sales
+CREATE TABLE Sale
 (
     SaleId integer primary key,
     PersonId integer references Person(personId) not null,
     Item text not null,
-    quantity integer,
+    Quantity integer,
     BillingAddress integer references Address(AddressId) not null,
     ShippingAddress integer references Address(AddressId),
     Ordered timestamp,
     Shipped timestamp
 );
 
-CREATE TABLE Donations 
+CREATE TABLE Donation
 (
 	DonationID integer primary key,
 	PersonId integer references Person(personId) not null,
-	email text not null,
-	Amount decimal,
-	Date timestamp
+	Amount decimal not null,
+	Date timestamp default current_timestamp
 );
 
 CREATE TABLE SaleItem 
 (
 	SaleItemID integer primary key,
 	SaleId integer references Sale(SaleId) not null,
-	name text not null,
-	Description text not null,
-	Contents text not null,
+	ItemName text not null,
+	Description text,
+	Contents text,
 	UnitPrice decimal not null,
-	Quanity decimal not null	
+	Quanity integer	
 );
-
-
 
 CREATE TABLE Event
 (
     EventID integer primary key,
     MemberID integer references Member(MemberId) not null,
 	AddressID integer references Address(AddressId) not null,
-    EventTitle text,
+    EventTitle text not null,
     EventDescription text,
     EventPrice decimal,
     EventNotes text,
-    IsVetted text
+    IsVetted boolean default false
 );
---drop table eventcomment; 
-
-CREATE TABLE EventComment
-(
-    EventCommentID integer primary key,
-    EventID integer references Event(EventId) not null,
-    CommentID integer references Comment(CommentId) not null
-);
-
 
 CREATE TABLE Comment
 (
     CommentID integer primary key,
     MemberID integer references Member(MemberId) not null,
-    Date timestamp,
-    Comment text
-    
+    Comment text,
+    Date timestamp default current_timestamp
 );
-
 
 CREATE TABLE EventComment
 (
@@ -112,26 +115,15 @@ CREATE TABLE Resource
     MemberID integer references Member(MemberId) not null,
     ResourceLink integer not null,
     Title text not null,
-    Description text not null,
+    Description text,
     Date timestamp,
-    IsVetted text
-    
+    IsVetted boolean default false
 );
-
 
 CREATE TABLE ResourceComment
 (
     ResourceCommentID integer primary key,
     ResourceID integer references Resource(ResourceID) not null,
     CommentID integer references Comment(CommentID) not null
-    
 );
-
-
-	
-	
-
-
-
-
 
