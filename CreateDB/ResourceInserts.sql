7c042af59c995580839a18d614b52ecdb60d3f9d
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2021-12-04 21:38:59 -0800

    Update ResourceInserts.sql

--- CreateDB/ResourceInserts.sql
+++ CreateDB/ResourceInserts.sql
@@ -3,9 +3,9 @@ INSERT INTO Resource
 VALUES (
  100, 
  1, 
- "https://www.pepnw.org/about-us/",
- "Partnership for Energy progress",
- "The Partnership for Energy Progress is a collaboration of utilities, farmers, workers, small and large businesses, and community advocates across the Northwest. Our goal is to communicate the work we do to provide reliable, affordable energy to homes and businesses, and highlight the progress we’re making to address climate change.", 
+ 'https://www.pepnw.org/about-us/',
+ 'Partnership for Energy progress',
+ The Partnership for Energy Progress is a collaboration of utilities, farmers, workers, small and large businesses, and community advocates across the Northwest. Our goal is to communicate the work we do to provide reliable, affordable energy to homes and businesses, and highlight the progress we’re making to address climate change.', 
  '09/12/2020 12:00 PM',
  false
 );
@@ -15,9 +15,9 @@ INSERT INTO Resource
 VALUES (
  101, 
  1, 
- "https://new.siemens.com/us/en/company/sustainability/environmental-action.html",
- "Siemens Company",
- "Siemens is supporting the Decade of Action to accelerate the adoption of sustainable technologies so our customers, our communities and our employees can lead the U.S. towards a low-carbon future", 
+ 'https://new.siemens.com/us/en/company/sustainability/environmental-action.html',
+ 'Siemens Company',
+ 'Siemens is supporting the Decade of Action to accelerate the adoption of sustainable technologies so our customers, our communities and our employees can lead the U.S. towards a low-carbon future', 
  '10/09/2020 12:00 PM',
  false
 );
@@ -27,9 +27,9 @@ INSERT INTO Resource
 VALUES (
  102, 
  1, 
- "https://www.unep-wcmc.org/",
- "UN Environment Program",
- "UN Environment Programme World Conservation Monitoring Centre is a world leader in biodiversity knowledge. It works with scientists and policy makers worldwide to place biodiversity at the heart of environment and development decision-making to enable enlightened choices for people and the planet.", 
+ 'https://www.unep-wcmc.org/',
+ 'UN Environment Program',
+ 'UN Environment Programme World Conservation Monitoring Centre is a world leader in biodiversity knowledge. It works with scientists and policy makers worldwide to place biodiversity at the heart of environment and development decision-making to enable enlightened choices for people and the planet.', 
  '10/18/2020 12:00 PM',
  false
 );
@@ -39,9 +39,9 @@ INSERT INTO Resource
 VALUES (
  103, 
  1, 
- "https://climate.nasa.gov/ ",
- "Nasa on Climate Change",
- "Explore a real-time data visualization of NASA’s Earth-orbiting satellites and the data they collect about climate change." 
+ 'https://climate.nasa.gov/ ',
+ 'Nasa on Climate Change',
+ 'Explore a real-time data visualization of NASA’s Earth-orbiting satellites and the data they collect about climate change.',
  '10/25/2020 12:00 PM',
  false
 );
